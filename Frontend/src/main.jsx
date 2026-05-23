552654b24253ff36eb065300c55dd5abcea977ed
Isha Tilotiya <ishatilotiya@gmail.com>
2026-05-23 10:22:40 +0530

    fix folder structure

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-    <Toaster position="top-right" toastOptions={{
-      style: { background: '#1e1e2e', color: '#cdd6f4', border: '1px solid #313244' }
-    }} />
-  </BrowserRouter>
+  </StrictMode>,
 )
