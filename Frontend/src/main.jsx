db8a945d1a24ff8156d2cbf6c9ac846c6507fea7
Isha Tilotiya <ishatilotiya@gmail.com>
2026-05-23 11:26:42 +0530

    fix router context

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+    <Toaster position="top-right" toastOptions={{
+      style: { background: '#1e1e2e', color: '#cdd6f4', border: '1px solid #313244' }
+    }} />
+  </BrowserRouter>
 )
